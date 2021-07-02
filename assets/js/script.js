f9149be81f2214739d9c9e9bbea415f4ae38087f
Kestle Olson <kestle1289@gmail.com>
2021-07-01 22:23:56 -0700

    input logged to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,37 @@
 var today = moment();
 $('#currentDay').text(today.format('MMMM Do YYYY, h:mm a'));
+
+//Retrieve Saved Plans
+function returnPlans (){
+    var storedPlans = JSON.parse(localStorage.getItem('savedPlans'));
+    console.log(storedPlans)
+    if (storedPlans !== null){
+        for (i=0; i < storedPlans.length; i++) {
+            retrievedPlans = storedPlans[i];
+            plans = retrievedPlans.plans;
+            hour = retrievedPlans.hour;
+        }
+    }
+}
+
+
+
+//Save Plans to local Storage 
+var saveBtn = $(".saveBtn")
+
+saveBtn.on("click",function () {    
+    var hour = $(this).parent().attr("id");
+    console.log(hour)
+    var plans = $(this).siblings(".plans").val() 
+    
+    var savedPlans = {
+        hour: hour,
+        plans: plans,
+    }
+    console.log(savedPlans)
+    localStorage.setItem('savedPlans',JSON.stringify(savedPlans));
+})
+
+
+
+//Color Coding Function 
